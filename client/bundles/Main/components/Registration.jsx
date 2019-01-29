8ba1d9c61f32f48aba2230bed5d9cce9371c7955
Aditya Naik <naik.aditya@gmail.com>
2019-01-29 12:39:41 +0100

    Adds axios, updates onSubmit method with post request to users/sign_up route

--- client/bundles/Main/components/Registration.jsx
+++ client/bundles/Main/components/Registration.jsx
@@ -1,20 +1,25 @@
 import React, { Component } from 'react'
 
+const axios = require('axios');
 
- function RegistrationForm() {
+
+ function RegistrationForm(props) {
   return (
     <div>
-        <label htmlFor="First name">First name</label>
-            <input name="First name" type="text"/> 
-        <label htmlFor="Last name">Last name</label>
-            <input name="Last name" type="text"/>
-        <label htmlFor="Email">Email</label>
-            <input name="Email" type="email"/>
-        <label htmlFor="password">Password</label>
-            <input name="Password" type="password"/>
-        <label htmlFor="password_confirmation">Password Confirmation</label>
-            <input name="Password confirmation" type="password"/>
-        <button name="Submit" type="submit">Submit</button>
+        <form onSubmit={props.onSubmit}>
+            <label htmlFor="First name">First name</label>
+                <input name="First name" type="text"/> 
+            <label htmlFor="Last name">Last name</label>
+                <input name="Last name" type="text"/>
+            <label htmlFor="Email">Email</label>
+                <input name="Email" type="email"/>
+            <label htmlFor="password">Password</label>
+                <input name="Password" type="password"/>
+            <label htmlFor="password_confirmation">Password Confirmation</label>
+                <input name="Password confirmation" type="password"/>
+            <button name="Submit" type="submit">Submit</button>
+        </form>
+        
     </div>
   )
 }
@@ -27,6 +32,18 @@ export class Registration extends Component {
             user_role: ''
         }
     }
+
+    onSubmit = () => {
+        event.preventDefault()
+    
+        axios({
+          method: 'post',
+          url: '/users/sign_up',
+          data: {
+            name: this.state.name
+          }
+        });
+      }
 
     onClick(event){
         event.preventDefault()
@@ -42,7 +59,7 @@ export class Registration extends Component {
         <h1>I am a</h1>
         <button name="user_role" value="Trainee" onClick = {this.onClick}>Trainee</button>
         <button name="user_role" value="Coach" onClick = {this.onClick}>Coach</button>
-        <RegistrationForm />
+        <RegistrationForm onSubmit={this.onSubmit}/>
       </div>
     )
   }
