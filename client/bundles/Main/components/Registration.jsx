620f9ee9d12ed9bb53156d4950a0d4a1e154bcb2
Aditya Naik <naik.aditya@gmail.com>
2019-01-29 12:29:20 +0100

    Adds registration form input elements

--- client/bundles/Main/components/Registration.jsx
+++ client/bundles/Main/components/Registration.jsx
@@ -1,12 +1,48 @@
 import React, { Component } from 'react'
 
+
+ function RegistrationForm() {
+  return (
+    <div>
+        <label htmlFor="First name">First name</label>
+            <input name="First name" type="text"/> 
+        <label htmlFor="Last name">Last name</label>
+            <input name="Last name" type="text"/>
+        <label htmlFor="Email">Email</label>
+            <input name="Email" type="email"/>
+        <label htmlFor="password">Password</label>
+            <input name="Password" type="password"/>
+        <label htmlFor="password_confirmation">Password Confirmation</label>
+            <input name="Password confirmation" type="password"/>
+        <button name="Submit" type="submit">Submit</button>
+    </div>
+  )
+}
+
+
 export class Registration extends Component {
+    constructor(){
+        super()
+        this.state = {
+            user_role: ''
+        }
+    }
+
+    onClick(event){
+        event.preventDefault()
+        this.setState ({
+            [event.target.id]: event.target.value
+        })
+    }
+
+
   render() {
     return (
       <div>
         <h1>I am a</h1>
-        <button>Trainee</button>
-        
+        <button name="user_role" value="Trainee" onClick = {this.onClick}>Trainee</button>
+        <button name="user_role" value="Coach" onClick = {this.onClick}>Coach</button>
+        <RegistrationForm />
       </div>
     )
   }
