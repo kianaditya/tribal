dab70de7e068419686e843ae116d6029fe996dbf
Gregory Kallai <gerg.kallai@gmail.com>
2019-02-04 20:01:05 +0100

    Adds react-facebook package, adds share button with placeholder share link
    Co-authored-by: Aditya Naik <naik.aditya@gmail.com>

--- client/bundles/Main/components/sessions/ShowSession.jsx
+++ client/bundles/Main/components/sessions/ShowSession.jsx
@@ -1,32 +1,36 @@
 import React, { Component } from 'react';
 import SessionCard from './SessionCard';
-
+import { FacebookProvider, ShareButton } from 'react-facebook';
 export class ShowSession extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			session: props.session
 		};
-	this.buyStripe = this.buyStripe.bind(this);
+		this.buyStripe = this.buyStripe.bind(this);
 	}
 
-	buyStripe(){
+	buyStripe() {
 		document.location.href = "/transactions/new" + "?sessionId=" + this.state.session.id;
 	}
 
 	render() {
 		return (
-			<div  className='main_container wrapper-col' >
+			<div className='main_container wrapper-col' >
 				<SessionCard session={this.state.session} />
 				<div>
-				<button className='button' onClick={this.buyStripe}>
+					<button className='button' onClick={this.buyStripe}>
 						Buy Session
 				</button>
+					<FacebookProvider appId="254630232098889">
+						<ShareButton className='button' href="https://index.hu/">
+							Share
+					</ShareButton>
+					</FacebookProvider>
 				</div>
 			</div>
 		);
 	}
-}
+};
 
 export default ShowSession;
-
